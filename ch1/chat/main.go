9dceeb7ad0d42f4ebb7a390f8d2c39c3e3b94716
taitai9847 <taitai9847@gmail.com>
2021-09-19 02:15:42 +0900

    done 2.6.3

--- ch1/chat/main.go
+++ ch1/chat/main.go
@@ -9,6 +9,9 @@ import (
 	"sync"
 	"text/template"
 
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 	"github.com/taitai9847/goblueprints/ch1/trace"
 )
 
@@ -22,12 +25,27 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
+
+	gomniauth.SetSecurityKey("uYSfFhQ2ZyPnzIPmJhnIEY7YLCzkgk3cLmBddanBLrvT2lvwUPqt0deDFpR3h9n6")
+	gomniauth.WithProviders(
+		// github.New("", "", "http://localhost:8080/auth/callback/github"),
+		google.New("1016034543021-b0tekt7ndko3u4haoefskkkka6ciog40.apps.googleusercontent.com", "9no0yUxHHkxoUK-jk5RcYY_9", "http://localhost:8080/auth/callback/google"),
+		// facebook.New("", "", "http://localhost:8080/auth/callback/facebook"),
+	)
+
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 
@@ -37,7 +55,9 @@ func main() {
 	http.Handle("/room", r)
 
 	go r.run()
+
 	log.Println("Starting web server on", *addr)
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
