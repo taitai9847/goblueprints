110b65b57e39e601b025008dfa5a9a8821f5127b
taitai9847 <taitai9847@gmail.com>
2021-11-14 14:00:50 +0900

    done ch7

--- ch7/meander/cmd/main.go
+++ ch7/meander/cmd/main.go
@@ -4,16 +4,31 @@ import (
 	"encoding/json"
 	"net/http"
 	"runtime"
+	"strconv"
+	"strings"
 
 	"github.com/taitai9847/goblueprints/ch7/meander"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	//meander.APIKey = 'TODO'
-	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
+	meander.APIKey = "GOOGLE_PLACES_API_KEY"
+	http.HandleFunc("/journeys", cors(func(w http.ResponseWriter, r *http.Request) {
 		respond(w, r, meander.Journeys)
-	})
+	}))
+	http.HandleFunc("/recommendations", cors(func(
+		w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		q.Lat, _ = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		q.Lng, _ = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		q.Radius, _ = strconv.Atoi(r.URL.Query().Get("radius"))
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	}))
+
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
 
@@ -23,4 +38,11 @@ func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
 		publicData[i] = meander.Public(d)
 	}
 	return json.NewEncoder(w).Encode(publicData)
+}
+
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		f(w, r)
+	}
 }
